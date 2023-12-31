2e3eb691b363d635dd41e351edad9c841c508666
Chris Bowen Jr <laylong@gmx.com>
2023-12-31 15:28:01 -0500

    Saving code to test a minimal bug replication

--- chimerik.py
+++ chimerik.py
@@ -1,125 +1,54 @@
 import sys
+from sly import Lexer, Parser
 
-class Chimerik:
-    def __init__(self, code):
-        self.code = code
-        self.line_nr = 0
-        self.token_feed = self.tokens()
-        self.returned_token = None
-        self.stack = []
-        self.variables = {}
-        self.token_list = ["trooden", "="]
+class Chimerik_Lexer(Lexer):
+    tokens = { ID, MUTABLE, TYPE, IMMUTABLE, NUMBER, ASSIGN, IF, ELSE, END }
+    literals = { '+', '-', '*', '/', '(', ')', '{', '}', '[', ']', '.', ';' }
 
-    def raise_error(self, message):
-        raise ValueError(f'Line {self.line_nr}: {message}')
+    ignore = ' \t'
+    ignore_comment = r'\\\\.'
+    ignore_newline = r'\n+'
+    
+    MUTABLE = r'imil'
+    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    ID['if'] = IF
+    ID['else'] = ELSE
+    ID['ston'] = IMMUTABLE
+    ID['int'] = TYPE
+    NUMBER = r'\d+'
+    ASSIGN = r'='
+    END = r';'
 
-    def tokens(self):
-        for line in self.code.strip().split('\n'):
-            self.line_nr += 1
-            for token in line.strip().split(' '):
-                if token in self.token_list:
-                    yield (token,)
-                elif token.isnumeric():
-                    yield ('number', int(token))
-                elif token[0].isalpha():
-                    yield ('identifier', token)
-                else:
-                    self.raise_error(f'Syntax Error: Invalid token {token}')
-            yield ('\n',)
+    def __init__(self):
+        self.para_nest_count = 0
+        self.curly_nest_count = 0
+        self.bracket_next_count = 0
+        self.line_number = 0
 
-    def next_token(self):
-        if self.returned_token:
-            token = self.returned_token
-            self.returned_token = None
-        else:
-            try:
-                token = next(self.token_feed)
-            except StopIteration:
-                token = None
-        return token
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.line_number += len(t.value)
 
-    def return_token(self, token):
-        if self.returned_token is not None:
-            raise RuntimeError("Cannot return a token")
-        self.returned_token = token
 
-    def parse_program(self):
-        if not self.parse_statement():
-            self.raise_error("Expected: statement")
-        token = self.next_token()
-        while token is not None:
-            self.return_token(token)
-            if not self.parse_statement():
-                self.raise_error("Expected: statement")
-            token = self.next_token()
-        return True
+    def error(self, t):
+        print(f"Line {self.line_number}: Bad character {t.value[0]}")
+        self.index += 1
 
-    def parse_statement(self):
-        if not self.parse_print_statement() and not self.parse_assignment():
-            self.raise_error("Expected: statement to be valid")
-        token = self.next_token()
-        if token[0] != '\n':
-            self.raise_error("Expected: end of line")
-        return True
+class Chimerik_Parser(Parser):
+    tokens = Chimerik_Lexer.tokens
 
-    def parse_assignment(self):
-        token = self.next_token()
-        if token[0] != 'identifier':
-            self.return_token(token)
-            return False
-        identifier = token[1]
-        token = self.next_token()
-        if token[0] != '=':
-            self.raise_error("Expected: =")
-        if not self.parse_expression():
-            self.raise_error("Expected: expression")
+    def __init__(self):
+        self.names = ()
 
-        self.variables[identifier] = self.stack_pop()
-        return True
-
-    def parse_print_statement(self):
-        token = self.next_token()
-        if token[0] != 'trooden':
-            self.return_token(token)
-            return False
-        if not self.parse_expression():
-            self.raise_error("Expected: expression")
-
-        value = self.stack_pop()
-        print(value)
-        return True
-
-    def parse_expression(self):
-        token = self.next_token()
-        if token[0] not in ['number', 'identifier']:
-            self.return_token(token)
-            return False
-
-        if token[0] == 'identifier':
-            if token[1] not in self.variables:
-                self.raise_error(f"Syntax Error: unknown variable {token[1]}")
-            else:
-                self.stack_push(self.variables[token[1]])
-
-        self.stack_push(token[1])
-        return True
-
-    def stack_push(self, arg):
-        self.stack.append(arg)
-
-    def stack_pop(self):
-        return self.stack.pop()
-
-    def run(self):
-        print(list(self.tokens()))
-        try:
-            return self.parse_program()
-        except ValueError as exc:
-            print(str(exc))
-            return False
+    @_("MUTABLE TYPE ID ASSIGN NUMBER END")
+    def numeric_assignment(self, p):
+        print(f"{p.ID} is a {p.MUTABLE} {p.TYPE} containing {p.NUMBER}")
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'rt') as source_file:
         code = source_file.read()
-    program = Chimerik(code)
-    program.run()
+    lexer = Chimerik_Lexer()
+    parser = Chimerik_Parser()
+    for tok in lexer.tokenize(code):
+        print(f"type={tok.type}: value={tok.value}")
+    result = parser.parse(lexer.tokenize(code))
